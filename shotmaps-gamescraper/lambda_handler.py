60de467e757e5efa8bf13d2f3fe31db7d6490d29
Matt Donders <mdonders@gmail.com>
2019-09-04 00:52:42 -0400

    First commit in this new branch of a split lambda with SNS trigger and layers.

---
+++ shotmaps-gamescraper/lambda_handler.py
@@ -0,0 +1,115 @@
+import json
+import logging
+import os
+from datetime import datetime
+
+from boto3 import client as boto3_client
+import hockey_scraper
+
+lambda_client = boto3_client("lambda")
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG) if os.environ.get("LOGLEVEL") == "DEBUG" else logger.setLevel(logging.INFO)
+
+# Test-based global variables
+TESTING = False
+TEST_TOPICS = [
+    "arn:aws:sns:us-east-1:024303108096:POC-NHLP3-Play-by-Play-Events",
+    "arn:aws:sns:us-east-1:627812672245:NHLP3-Play-by-Play-Events",
+]
+
+
+def get_game_id(event: dict):
+    """ Takes the event & tries to determine & validate the Game ID.
+
+    Args:
+        event: event passed into the AWS Lambda
+
+    Returns:
+        dict: {status, game_id, msg}
+    """
+
+    global TESTING
+
+    # Get the Game ID from the event parameter passed into the handler or via environment variable
+    print(event)
+
+    game_id = event.get("game_id")
+    game_id = game_id if game_id is not None else os.environ.get("GAMEID")
+
+    if not game_id and event.get("Records") is not None:
+        logging.info(
+            "Game ID is still none - this is an SNS message & the gameId is contained in the message field."
+        )
+        sns_record = event.get("Records")
+        sns_record = sns_record[0]
+        sns_event = sns_record.get("Sns")
+        topic = sns_event.get("TopicArn")
+
+        # Set TESTING global based on topic name
+        TESTING = False if topic in TEST_TOPICS else True
+
+        msg = sns_event.get("Message")
+        msg_json = json.loads(msg)
+        game_id = msg_json.get("game_id")
+
+    if not game_id or game_id is None:
+        logging.error("An NHL Game ID is required for this script to run.")
+        return {"status": False, "msg": "An NHL Game ID is required for this script to run."}
+
+    # Validate Game ID meets all criteria to actually run this script
+    # In-Season game validation will happen at the API endpoint
+    game_id = str(game_id)
+    season = game_id[0:4]
+    game_type = game_id[4:6]
+    game_number = game_id[6:10]
+
+    if int(season) > datetime.now().year:
+        return {"status": False, "msg": "Invalid season detected in the specified Game ID."}
+
+    if int(game_type) > 4:
+        return {"status": False, "msg": "Invalid game type detected in the specified Game ID."}
+
+    if int(game_number) > 1271:
+        return {"status": False, "msg": "Invalid game number detected in the specified Game ID."}
+
+    # If all validations pass, return our game_id
+    return {"status": True, "game_id": game_id}
+
+
+def lambda_handler(event, context):
+    global TESTING
+
+    LAMBDA_GENERATOR = os.environ.get("LAMBDA_GENERATOR")
+    game_id_dict = get_game_id(event)
+    game_id_status = game_id_dict["status"]
+
+    if not game_id_status:
+        logging.error(game_id_dict["msg"])
+        return {"status": False, "msg": game_id_dict["msg"]}
+
+    # If status is True, set the game_id variable
+    game_id = game_id_dict["game_id"]
+
+    scraped_data = hockey_scraper.scrape_games([game_id], False, data_format="Pandas")
+    pbp = scraped_data.get("pbp")
+
+    # fmt: off
+    cols_to_drop = ['awayPlayer1', 'awayPlayer1_id', 'awayPlayer2', 'awayPlayer2_id', 'awayPlayer3',
+            'awayPlayer3_id', 'awayPlayer4', 'awayPlayer4_id', 'awayPlayer5', 'awayPlayer5_id', 'awayPlayer6',
+            'awayPlayer6_id', 'homePlayer1', 'homePlayer1_id', 'homePlayer2', 'homePlayer2_id', 'homePlayer3',
+            'homePlayer3_id', 'homePlayer4', 'homePlayer4_id', 'homePlayer5', 'homePlayer5_id', 'homePlayer6',
+            'homePlayer6_id', 'Description', 'Home_Coach', 'Away_Coach']
+    # fmt: on
+
+    pbp = pbp.drop(cols_to_drop, axis=1)
+    pbp.columns = map(str.lower, pbp.columns)
+
+    pbp_json = pbp.to_json()
+    payload = {"pbp_json": pbp_json, "testing": TESTING}
+
+    invoke_response = lambda_client.invoke(
+        FunctionName=LAMBDA_GENERATOR, InvocationType="Event", Payload=json.dumps(payload)
+    )
+
+    print(invoke_response)
---
+++ shotmaps-generator-sendtweet/lambda_handler.py
@@ -0,0 +1,181 @@
+import json
+import logging
+import math
+import os
+
+import tweepy
+import pandas as pd
+
+# Custom Imports
+import clean_pbp
+import shotmap
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ordinal = lambda n: "%d%s" % (n, "tsnrhtdd"[(math.floor(n / 10) % 10 != 1) * (n % 10 < 4) * n % 10 :: 4])
+
+
+def send_shotmap_tweet(testing: bool, completed_path: str, tweet_text: str):
+    """ Takes a completed shotmap path & some text and sends out a tweet.
+        Twitter keys are stored in environment variables.
+
+    Args:
+        completed_path: The path to the completed shotmap
+        tweet_text: Any text to send alongside the string
+
+    Returns:
+        True if tweet sent or TweepyError if failed
+    """
+
+    # Get keys from environment variables based on if this is a "test-run" or not.
+    if testing:
+        consumer_key = os.environ.get("debug_twtr_consumer_key")
+        consumer_secret = os.environ.get("debug_twtr_consumer_secret")
+        access_token = os.environ.get("debug_twtr_access_token")
+        access_secret = os.environ.get("debug_twtr_access_secret")
+    else:
+        consumer_key = os.environ.get("wtr_consumer_key")
+        consumer_secret = os.environ.get("twtr_consumer_secret")
+        access_token = os.environ.get("twtr_access_token")
+        access_secret = os.environ.get("twtr_access_secret")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
+
+    try:
+        if api.update_with_media(completed_path, tweet_text):
+            return True
+    except tweepy.error.TweepError as e:
+        return e
+
+
+def get_team_from_abbreviation(abbreviation: str):
+    """ Takes a team abbreviation and returns the team short name.
+
+    Args:
+        abbreviation: 3-character team abbreviation
+
+    Returns:
+        {team_name, short_name}: A dictionary of team_name & short name
+    """
+
+    team_name_dict = {
+        "NJD": {"team_name": "New Jersey Devils", "short_name": "Devils"},
+        "NYI": {"team_name": "New York Islanders", "short_name": "Islanders"},
+        "NYR": {"team_name": "New York Rangers", "short_name": "Rangers"},
+        "PHI": {"team_name": "Philadelphia Flyers", "short_name": "Flyers"},
+        "PIT": {"team_name": "Pittsburgh Penguins", "short_name": "Penguins"},
+        "BOS": {"team_name": "Boston Bruins", "short_name": "Bruins"},
+        "BUF": {"team_name": "Buffalo Sabres", "short_name": "Sabres"},
+        "MTL": {"team_name": "Montréal Canadiens", "short_name": "Canadiens"},
+        "OTT": {"team_name": "Ottawa Senators", "short_name": "Senators"},
+        "TOR": {"team_name": "Toronto Maple Leafs", "short_name": "Maple Leafs"},
+        "CAR": {"team_name": "Carolina Hurricanes", "short_name": "Hurricanes"},
+        "FLA": {"team_name": "Florida Panthers", "short_name": "Panthers"},
+        "TBL": {"team_name": "Tampa Bay Lightning", "short_name": "Lightning"},
+        "WSH": {"team_name": "Washington Capitals", "short_name": "Capitals"},
+        "CHI": {"team_name": "Chicago Blackhawks", "short_name": "Blackhawks"},
+        "DET": {"team_name": "Detroit Red Wings", "short_name": "Red Wings"},
+        "NSH": {"team_name": "Nashville Predators", "short_name": "Predators"},
+        "STL": {"team_name": "St. Louis Blues", "short_name": "Blues"},
+        "CGY": {"team_name": "Calgary Flames", "short_name": "Flames"},
+        "COL": {"team_name": "Colorado Avalanche", "short_name": "Avalanche"},
+        "EDM": {"team_name": "Edmonton Oilers", "short_name": "Oilers"},
+        "VAN": {"team_name": "Vancouver Canucks", "short_name": "Canucks"},
+        "ANA": {"team_name": "Anaheim Ducks", "short_name": "Ducks"},
+        "DAL": {"team_name": "Dallas Stars", "short_name": "Stars"},
+        "LAK": {"team_name": "Los Angeles Kings", "short_name": "Kings"},
+        "SJS": {"team_name": "San Jose Sharks", "short_name": "Sharks"},
+        "CBJ": {"team_name": "Columbus Blue Jackets", "short_name": "Blue Jackets"},
+        "MIN": {"team_name": "Minnesota Wild", "short_name": "Wild"},
+        "WPG": {"team_name": "Winnipeg Jets", "short_name": "Jets"},
+        "ARI": {"team_name": "Arizona Coyotes", "short_name": "Coyotes"},
+        "VGK": {"team_name": "Vegas Golden Knights", "short_name": "Golden Knights"},
+    }
+
+
+def lambda_handler(event, context):
+    # logging.info(event)
+    testing = event.get("testing")
+
+    # Get the JSON-serialized DataFrame from the payload & convert back to a DataFrame
+    pbp_json = event.get("pbp_json")
+    pbp_json = json.dumps(pbp_json) if isinstance(pbp_json, dict) else pbp_json
+    pbp_df = pd.read_json(pbp_json)
+
+    # Get Home & Away team names from DF
+    home_team = pbp_df.home_team.unique()[0]
+    away_team = pbp_df.away_team.unique()[0]
+
+    cols = " ".join(pbp_df.columns)
+    df_stats = f"{cols}\nDF Stats: {len(pbp_df.index)} rows x {len(pbp_df.columns)} columns\nHome: {home_team}\nAway: {away_team}"
+    print(df_stats)
+
+    # Fix Team Abbreviations (in both DFs)
+    team_corrections = {"L.A": "LAK", "N.J": "NJD", "S.J": "SJS", "T.B": "TBL"}
+    pbp_df.replace(
+        {"ev_team": team_corrections, "home_team": team_corrections, "away_team": team_corrections},
+        inplace=True,
+    )
+
+    # Fix elapsed seconds first
+    pbp_df = clean_pbp.fix_seconds_elapsed(pbp_df)
+    pbp_df = clean_pbp.clean_df(pbp_df)
+
+    # Then run all other cleaning & stat generation functions at once
+    pbp_df = clean_pbp.run_all_stats(pbp_df)
+
+    # Filter out for Corsi-only events
+    logging.info("Removing all non-corsi events as they should not be graphed.")
+    pbp_df = pbp_df.loc[pbp_df["is_corsi"] == 1]
+    print(pbp_df.event.unique())
+
+    pbp_df.replace(to_replace=["", "NA"], value=pd.np.nan, inplace=True)
+
+    logging.info("Fixing periods & splitting dataframes into two teams.")
+    pbp_df = clean_pbp.fix_df_periods(pbp_df)
+
+    logging.info("Extracting only corsi events to graph on the shotmap.")
+    home_df, away_df = clean_pbp.split_df(pbp_df, home_team)
+
+    logging.debug("Home DataFrame:")
+    logging.debug(home_df[["xc", "yc"]])
+
+    logging.debug("Away DataFrame:")
+    logging.debug(away_df[["xc", "yc"]])
+
+    # Generate the Shotmap
+    completed_path = shotmap.generate_shotmap(home_df=home_df, away_df=away_df)
+
+    # Get items from the Datafrmae for Description Purposes
+    last_row = pbp_df.iloc[-1]
+    period = last_row.period
+    period_ordinal = ordinal(period)
+    home_score = last_row.home_score
+    away_score = last_row.away_score
+
+    home_team = get_team_from_abbreviation(home_team)
+
+    if home_score > away_score:
+        tweet_text = (
+            f"At the end of the {period_ordinal} period the {home_team.get('short_name')} "
+            f"lead the {home_team.get('short_name')} by a score of {home_score} to {away_score}."
+        )
+    elif away_score > home_score:
+        tweet_text = (
+            f"At the end of the {period_ordinal} period the {home_team.get('short_name')} "
+            f"trail the {home_team.get('short_name')} by a score of {home_score} to {away_score}."
+        )
+    else:
+        tweet_text = (
+            f"At the end of the {period_ordinal} period the {home_team.get('short_name')} "
+            f"and the {home_team.get('short_name')} are tied at {home_score}."
+        )
+
+    # Send the completed shotmap tweet
+    status = send_shotmap_tweet(
+        testing=testing, completed_path=completed_path, tweet_text="Sent from our new AWS Lambda function!"
+    )
+
